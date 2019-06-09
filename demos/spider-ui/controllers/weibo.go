7caf34a4b17e4b6c3e9277324984e6e5c2436993
hq <hq_cml@163.com>
2019-06-09 22:14:44 +0800

    Spider搜索引擎页面Demo: 搜索

--- demos/spider-ui/controllers/weibo.go
+++ demos/spider-ui/controllers/weibo.go
@@ -2,11 +2,21 @@ package controllers
 
 import (
 	"github.com/hq-cml/spider-face/core"
+	"net/http"
+	"strings"
+	"io/ioutil"
+	"fmt"
+	"github.com/hq-cml/spider-face/utils/helper"
 )
+
+var SpiderEngineAddr string
+var SpiderEngineDb string
+var SpiderEngineTable string
 
 //创建标准的Controller，必须以Controller后缀结尾
 //并实现core.Controller接口
 type WeiboController struct {
+
 	spdrp core.SpiderRoundtrip
 	entries []core.RouteEntry
 }
@@ -31,4 +41,32 @@ func (ic *WeiboController) SetRouteEntries(entries []core.RouteEntry) {
 //首页展示
 func (ic *WeiboController) IndexAction(rp core.Roundtrip) {
 	rp.Display("weibo/index")
+}
+
+//搜索接口
+func (ic *WeiboController) SearchAction(rp core.Roundtrip) {
+	keyword := rp.Param("keyword")
+
+	//创建自定义的client进行搜索
+	client := &http.Client{}
+	query := map[string]string{
+		"database" : SpiderEngineDb,
+		"table" : SpiderEngineTable,
+		"value" : keyword,
+	}
+	req, _ := http.NewRequest("GET",
+		fmt.Sprintf("http://%s/_search", SpiderEngineAddr), strings.NewReader(helper.JsonEncode(query)))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		msg := fmt.Sprintf("Search Error... %v", err)
+		rp.Redirect(fmt.Sprintf("/err?msg=%s", msg))
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	rp.Echo(string(body))
 }
--- demos/spider-ui/ui.go
+++ demos/spider-ui/ui.go
@@ -18,7 +18,14 @@ func main() {
 	wc.SetRouteEntries([]core.RouteEntry{
 		{Method: http.MethodGet,  Location: "/",      		 Action:"IndexAction",},
 		{Method: http.MethodGet,  Location: "/index", 		 Action:"IndexAction",},
+		{Method: http.MethodPost, Location: "/_search", 	 Action:"SearchAction",},
+		{Method: http.MethodGet,  Location: "/_list", 	     Action:"ListAction",},
+		{Method: http.MethodGet,  Location: "/_detail",      Action:"DetailAction",},
 	})
+
+	controllers.SpiderEngineAddr = "192.168.110.133:9528"
+	controllers.SpiderEngineDb = "sp_db"
+	controllers.SpiderEngineTable = "weibo"
 
 	//注册controller
 	if err := spd.RegisterController([]core.Controller{
